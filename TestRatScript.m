95f2071b03d853aa95334493417764e59246b5ff
Seth <60856766+sethupavan12@users.noreply.github.com>
2021-09-30 08:39:58 +0100

    changed Test script

--- TestRatScript.m
+++ TestRatScript.m
@@ -1,5 +1,6 @@
 parpool();
 project = openProject('RAT.prj');
-%reflectivity_calculation_compile_script;
-suite = matlab.unittest.TestSuite.fromProject(project)
+reflectivity_calculation_compile_script;
+suite = matlab.unittest.TestSuite.fromProject(project);
 results = run(suite)
+% SHUT DOWN PARALLEL POOL AFTER 

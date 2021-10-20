ce966ced83268afa8eb37a1239dffdd89bc279af
Arwel Hughes <arwel.hughes@stfc.ac.uk>
2021-10-20 12:38:20 +0100

    Fixed path issue in BuildScript

--- utilities/plotting/plotBayes.m
+++ utilities/plotting/plotBayes.m
@@ -3,12 +3,12 @@ function plotBayes(problem,results,varargin)
 
         figure(1); clf; plotRefSLD(problem,results)
 
-        h2 = figure(3); clf; subplot(1,2,1)
+        h2 = figure(3); clf; %subplot(1,2,1)
         sf = results.contrastParams.scalefactors;
         bayesShadedPlot_dev(problem,results,sf,'q4',true,'fit','max','KeepAxes',true)
         
-        subplot(1,2,2);
-        bayesSLDShadedPlot(problem,results,'fit','max','KeepAxes',true)
+%         subplot(1,2,2);
+%         bayesSLDShadedPlot(problem,results,'fit','max','KeepAxes',true)
         
         h3 = figure(4); clf
         mcmcplot(results.chain,[],results.fitNames,'hist');

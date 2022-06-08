b97876c1a78af8b58269f0e92cf300b2ebea419f
Seth <60856766+sethupavan12@users.noreply.github.com>
2022-06-08 15:14:47 +0100

    Added devOps docs

--- documentation/source/conf.py
+++ documentation/source/conf.py
@@ -46,7 +46,7 @@ release = '0.1'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinxcontrib.matlab', 'sphinx.ext.autodoc']
+extensions = ['sphinxcontrib.matlab', 'sphinx.ext.autodoc', 'sphinx_design']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']

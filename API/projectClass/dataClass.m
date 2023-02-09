1e7a1f21d8f0dca1aa1acab1fece8a94481f02f9
Stephen Nneji <steve.nneji@gmail.com>
2023-02-09 13:35:11 +0000

    Refactors and adds tests for the data class

--- API/projectClass/dataClass.m
+++ API/projectClass/dataClass.m
@@ -1,75 +1,77 @@
 classdef dataClass < handle
-    
     % A container class for holding data
     properties
-        
-        dataTable;
-        dataCount = 0;
-        autoDataNameCounter;
-        defaultSimMin = 0.005;
-        defaultSimMax = 0.7;
-        
+        dataTable
+    end
+
+    properties (Access = private)   
+        autoDataNameCounter = 1
+        defaultSimMin = 0.005
+        defaultSimMax = 0.7
     end
     
+    properties (Dependent)
+        dataCount
+    end
+
     methods
         
-        function obj = dataClass(startCell)
-            
-            sz = [1 4];
+        function obj = dataClass(varargin)
+            % Creates a Data object. The argument should be contents of the first
+            % dataset which should consists of a name (string), data (N by 3 array), 
+            % dataRange (1 by 2 or empty array), and simRange (1 by 2 or empty array). 
+            %
+            % data = dataClass('data_name',  [],  [], []);
+            sz = [0 4];
             varTypes = {'string','cell','cell','cell'};
             varNames = {'Name','Data','Data Range','Simulation Range'};
-            obj.dataTable = table('Size',sz,'VariableTypes',varTypes,'VariableNames',varNames);
-            
-            if isempty(startCell{4}{:})
-                startCell{4} = {[obj.defaultSimMin obj.defaultSimMax]};
-            end
-            
-            obj.dataTable(1,:) = startCell;%{'Substrate Roughness',1 3 5,true,'uniform',0,Inf};
-            obj.dataCount = 1;
-            obj.autoDataNameCounter = 1;
-            
+            obj.dataTable = table('Size',sz,'VariableTypes',varTypes,'VariableNames',varNames); 
+            obj.addData(varargin{:}); 
         end
         
-        function obj = addData(obj,varargin)
-            
-            if isempty(varargin{:})
+        function count = get.dataCount(obj)
+            count = height(obj.dataTable);
+        end
+
+        function obj = addData(obj, varargin)
+            % Adds a dataset to the data table. Default values are used if no 
+            % arguments are provided, otherwise a subset of the arguments can 
+            % be provided.
+            % The following are assumed from number of arguments: 
+            % for 1 input, the name only is provided
+            % for 2 inputs, the name and data are provided
+            % for 4 inputs, the name, data, data range, and simulation range are provided      
+            %
+            % data.addData();
+            % data.addData('Sim');
+            % data.addData('Sim', ones(5, 3));
+            % data.addData('Sim', ones(5, 3), [0.2, 0.9],  [-3, 6]);
+            if isempty(varargin)
                 
                 % Nothing supplied - add empty data row
-                nameVal = obj.autoDataNameCounter();
-                newName = sprintf('New data %d', nameVal);
+                newName = sprintf('New data %d', obj.autoDataNameCounter());
                 
                 newDataRange = [];
                 newSimRange = [obj.defaultSimMin, obj.defaultSimMax];
                 
                 newRow = {newName, [], newDataRange, newSimRange};
                 appendNewRow(obj,newRow);
-            else
-                
-                inputs = varargin{:};
-                
+            else   
+                inputs = varargin;
+                newName = inputs{1};
+                if ~ischar(newName)
+                    throw(invalidType('First input is expected to be a data name (char)'));
+                end
+                newData = [];
+                newDataRange = [];
+                newSimRange = [obj.defaultSimMin, obj.defaultSimMax];
                 % Check length of added data
                 switch length(inputs)
-                    
-                    case 1
-                        
-                        % One input supplied - assume just name provided
-                        newName = inputs{1};
-                        if ~ischar(newName)
-                            error('Single input is expected to be a data name');
-                        end
-                        
-                        newDataRange = [];
-                        newSimRange = [obj.defaultSimMin, obj.defaultSimMax];
-                        
-                        newRow = {newName, [], newDataRange, newSimRange};
-                        %newUserDataRow = {'none',[0,0],[obj.defaultSimMin,obj.defaultSimMax]};
-                        appendNewRow(obj,newRow);%,newUserDataRow);
-                        
-                    case 2
-                        
+                    case 1   
+                    case 2  
                         % Two inputs supplied - assume both name and data
                         % supplied;
-                        newName = inputs{1};
+                        % newName = inputs{1};
                         newData = inputs{2};
                         
                         newDataX = newData(:,1);
@@ -77,48 +79,51 @@ classdef dataClass < handle
                         newMax = newDataX(end);
                         
                         newDataRange = [newMin newMax];
-                        newSimRange = [newMin newMax];
-                        
-                        newRow = {newName, newData, newDataRange, newSimRange};
-                        %newUserDataRow = {'data',[newMin,newMax],[newMin,newMax]};
-                        appendNewRow(obj,newRow);% ,newUserDataRow);
-                        
-                    case 4
-                        
+                        newSimRange = [newMin newMax];                       
+                    case 4   
                         % Four inputs = assume data and simulation ranges also
                         % supplied
-                        disp('todo');
-                        
-                    otherwise
-                        
+                        newData = inputs{2};                       
+                        newDataRange = inputs{3};
+                        if ~isempty(inputs{4})
+                            newSimRange = inputs{4};
+                        end
+                    otherwise  
                         % Other length of inputs is not recognised
-                        error('Unrecognised input into addData');
-                        
+                        throw(invalidNumberOfInputs('Unrecognised input into addData'));
+                      
                 end
-            end
-            
+
+                appendNewRow(obj, {newName, newData, newDataRange, newSimRange}); 
+            end 
         end
         
         function nameChanged = setData(obj, varargin)
-           
-            % Set the value of an existing data object
-            inputs = varargin{:};
+            % Sets the values of an existing dataset. Expects the
+            % index or name of parameter and keyword/value pairs to set
+            %
+            % data.setData(2, 'name', 'new_name');
+            inputs = varargin;
             dataNames = obj.dataTable{:,1};
             
             % Always need three or more inputs to set data value
             if length(inputs) < 3
-                error('At least three inputs expected into ''setData''');
+                throw(invalidNumberOfInputs('At least three inputs expected into ''setData'''));
             end
                 
             % First input needs to be a data number or name
             whichData = inputs{1};
             if isnumeric(whichData)
                 if (whichData > obj.dataCount) || (whichData < 1)
-                    error('Data %d is not recognised', whichData);
+                    throw(indexOutOfRange(sprintf('The index %d is not within the range 1 - %d', whichData, obj.dataCount)));
+                    %error('Data %d is not recognised', whichData);
                 end
             elseif ischar(whichData)
                 if ~strcmpi(whichData,dataNames)
-                    error('Data name %s not recognised', whichData);
+                    % error('Data name %s not recognised', whichData);
+                    throw(nameNotRecognised(sprintf('Data object name %s not recognised', whichData)));
+                else
+                    whichData = find(strcmpi(whichData,dataNames));
                 end
             end
             
@@ -148,87 +153,67 @@ classdef dataClass < handle
             % so call the relevant set method for these (which will carry
             % out some additional checks)
             
-            nameChanged = []; % Flag wich is passed up to the calling function (to change contrasts if necessary)
-            
-            % Pass numeric identifyer for data into subfunctions
-            if ~isnumeric(whichData)
-                whichData = find(strcmpi(whichData,dataNames));
-            end
+            nameChanged = []; % Flag which is passed up to the calling function (to change contrasts if necessary)
             
             if ~isempty(results.name)
                 nameChanged = obj.setDataName(whichData,results.name);
             end
             
-            
             if ~isempty(results.data)
-                obj.setDataValues(whichData,results.data);
+                obj.dataTable{whichData, 2} = {results.data};
             end
             
             if ~isempty(results.dataRange)
-                obj.setDataRange(whichData,results.dataRange);
+                obj.dataTable{whichData, 3} = {results.dataRange};
             end
             
             if ~isempty(results.simRange)
-                obj.setSimRange(whichData,results.simRange);
+                obj.dataTable{whichData, 4} = {results.simRange};
             end
 
         end
         
-        function obj = setDataRange(obj,whichData,range)
-            
-            r = [range(1) range(2)];
-            obj.dataTable{whichData,3} = {range};
-        
-        end
-        
-        function obj = setSimRange(obj,whichData,range)
-            
-            r = [range(1) range(2)];
-            obj.dataTable{whichData,4} = {range};           
-            
-            
-        end
-
-        
         function nameChanged = setDataName(obj,whichData,name)
-            
-            % Name must be a char and not an existing name
+            % Sets the name of an existing dataset. Expects index of data  
+            % and the new name. Name must be a char and not an existing name.
+            % Returns a structure with the new name and old name
+            %
+            % names = data.setDataName({2, 'new name'});
             if ~ischar(name)
-                nameChanged = [];
-                error('Name must be a string / character array');
+                throw(invalidType('Name must be a character array'));
             end
             
             existingNames = obj.getDataNames;
-            if strcmpi(name,existingNames)
-                nameChanged = [];
-                error('Duplicate data names are not allowed');
+            if any(strcmpi(name,existingNames))
+                throw(duplicateName('Duplicate data names are not allowed'));
             end
             
             % Set the relevant name
             nameChanged.oldName = obj.dataTable{whichData,1};
             nameChanged.newName = name;
-            obj.dataTable{whichData,1} = {name};
-            
+            obj.dataTable{whichData,1} = {name};   
         end
-        
         
         function names = getDataNames(obj)
-            
-           names = obj.dataTable{:,1}; 
-            
+            % Returns a N x 1 cell array of names of the datasets 
+            % in the table. 
+            % 
+            % names = data.getDataNames();   
+           names = obj.dataTable{:,1};     
+        end  
+
+        function removeData(obj, row)
+            % Removes a data entry from the data table. Expects the 
+            % index or array of indices of dataset(s) to remove.
+            %
+            % data.removeData(2)
+            tab = obj.dataTable;
+            tab(row,:) = [];
+            obj.dataTable = tab;
         end
-        
-        function obj = setDataValues(obj,whichData,data)
-            
-            obj.dataTable{whichData,2} = {data};
-   
-        end
-        
-        
-        
+
         function displayDataObject(obj)
-            
-            % Display the table object. The actual obj.dataTable has the 
+            % Displays the table object. The actual obj.dataTable has the 
             % format {string, cell, double, double}, but for display we 
             % make a table that is all strings.
             
@@ -277,16 +262,18 @@ classdef dataClass < handle
 
             disp(displayTable);
             fprintf('\n');
- 
         end
-       
-        
+    end
+    methods(Access = protected)  
         function obj = appendNewRow(obj,newRow)
-            
+            % Appends a new row to the table. Expects a cell array  
+            % with row values to append
+            % 
+            % obj.appendNewRow({'name', [], [], []})
             tab = obj.dataTable;
             newName = newRow{1};
             if any(strcmpi(newName,tab{:,1}))
-                error('Duplicate data names not allowed');
+                throw(duplicateName('Duplicate data names not allowed'));
             end
             
             % Carry out checks of Data type and ranges
@@ -297,65 +284,51 @@ classdef dataClass < handle
             if ~isempty(data)   % Data supplied
                 
                 if ~isnumeric(data)
-                    error('Data must be a numeric array');
+                    throw(invalidType('Data must be a numeric array'));
                 end
                 
                 if ((~isnumeric(dataRange)) || any(size(dataRange) ~= [1,2]) || any(size(simRange) ~= [1,2]))
-                    error('Data range and sim range must be [1 x 2] numeric arrays');
+                    throw(invalidType('Data range and sim range must be [1 x 2] numeric arrays'));
                 end
 
                 dataX = data(:,1);              % First column is always Q
                 realDataRange = [dataX(1),dataX(end)];
                 
                 if dataRange(1) < realDataRange(1)
-                    warning('Data range can''t be less than data min - resetting');
+                    warning('dataRange(1) can''t be less than min data value - resetting');
                     dataRange(1) = realDataRange(1);
                 end
                 
                 if dataRange(2) > realDataRange(2)
-                    warning('Data range can''t be more than data max - resetting');
+                    warning('dataRange(2) can''t be more than max data value - resetting');
+                    dataRange(2) = realDataRange(2);
+                end
+                
+                if dataRange(1) > dataRange(2)
+                    warning('Data range min must not be greater than max - resetting');
+                    dataRange(1) = realDataRange(1);
+                    dataRange(2) = realDataRange(2);
                 end
                 
                 if simRange(1) > realDataRange(1)
-                    warning('Sim min range must be inside data range - resetting');
+                    warning('simRange(1) must be less than data range - resetting');
                     simRange(1) = realDataRange(1);
                 end
                 
                 if simRange(2) < realDataRange(2)
-                    warning('Sim max range must be inside data range - resetting');
+                    warning('simRange(2) must be greater than data range - resetting');
                     simRange(2) = realDataRange(2);
                 end
-                
             end
             
             row = {{newName}, {data}, {dataRange}, {simRange}};
             tab = [tab ; row];
             obj.dataTable = tab;
-            obj.dataCount = obj.dataCount + 1;
-            obj.autoDataNameCounter = obj.autoDataNameCounter + 1;
-            
-        end
-        
+            obj.autoDataNameCounter = obj.autoDataNameCounter + 1;    
+        end  
     end
-    
 end
 
-function inputBlock = parseDataInput(inputVals)
 
-    defaultName = '';
-    defaultData = [];
-    defaultRange = [obj.defaultSimMin obj.defaultSimMax];
-    defaultSimRange = [obj.defaultSimMin obj.defaultSimMax];
-
-    p = inputParser;
-    addParameter(p,'name', defaultName, @ischar);
-    addParameter(p,'data', defaultData, @isnumeric);
-    addParameter(p,'data range', defaultRange, @(a) validateattributes(a,{'double'},{'size',[1 2]}));
-    addParameter(p,'sim range', defaultSimRange, @(a) validateattributes(a,{'double'},{'size',[1 2]}));
-
-    parse(p,inputVals{:});
-    inputBlock = p.Results;
-
-end
 
     
---
+++ tests/testDataClass.m
@@ -0,0 +1,164 @@
+classdef testDataClass < matlab.unittest.TestCase   
+    
+    properties
+        data
+        datasets
+    end
+
+    methods(TestMethodSetup)
+        function createData(testCase)
+            testCase.datasets = {
+                    %  Name            data          data range  simulation range 
+                    'Bilayer / D2O',   [],          [],        [];
+                    'Bilayer / SMW',   repmat(-1:1, 3, 1)',   [-1, 1],       [-2, 2];
+                    'Bilayer / H2O',   ones(5, 3),    [0.2, 0.9],    [-3, 6];
+                };
+            testCase.data = dataClass();
+        end
+    end
+    methods
+        function addDataSets(testCase)
+            % Adds datasets for the tests
+            for i = 1:size(testCase.datasets, 1)
+                testCase.data.dataTable(i+1, :) = {testCase.datasets(i, 1), {testCase.datasets(i, 2)},...
+                                                   {testCase.datasets(i, 3)}, {testCase.datasets(i, 4)}};
+            end
+        end
+    end
+
+    methods (Test)
+        function testCreation(testCase)
+            % Tests data class can be created and the first dataset is set correctly 
+            newData = dataClass();
+            default = {"New data 1", {[]}, {[]}, {[0.0050, 0.7000]}};
+            for i = 1:4
+                testCase.verifyEqual(newData.dataTable{1, i}, default{i}, 'Data default not set correctly');
+            end
+            testCase.verifySize(newData.dataTable, [1, 4], 'Data has wrong dimension');
+            
+            newData = dataClass(testCase.datasets{2, :});
+            default = {"Bilayer / SMW", {repmat(-1:1, 3, 1)'}, {[-1, 1]}, {[-2, 2]}};
+            for i = 1:4
+                testCase.verifyEqual(newData.dataTable{1, i}, default{i}, 'Data default not set correctly');
+            end
+            testCase.verifySize(newData.dataTable, [1, 4], 'Data has wrong dimension');
+        end
+
+        function testGetNames(testCase)
+            % Tests getDataNames returns correctly  
+            names = testCase.data.getDataNames();
+            testCase.verifyEqual(names, "New data 1", 'Start data not set correctly');
+            testCase.verifySize(names, [1, 1], 'data has wrong dimension');
+
+            testCase.addDataSets();
+            names = testCase.data.getDataNames();
+            testCase.verifyEqual(names, ["New data 1"; convertCharsToStrings(testCase.datasets(:, 1))], 'Start data not set correctly');
+            testCase.verifySize(names, [size(testCase.datasets, 1)+1, 1], 'data has wrong dimension');
+        end
+
+        function testAddData(testCase)
+            % Checks that data can be added                    
+            testCase.data.addData('Sim 2', ones(4, 3));
+            testCase.verifySize(testCase.data.dataTable, [2, 4], 'data has wrong dimension');
+            testCase.verifyEqual(testCase.data.dataTable{:, 1}, ["New data 1"; "Sim 2"], 'addData method not working');
+            testCase.data.addData('Sim 3');
+            testCase.verifySize(testCase.data.dataTable, [3, 4], 'data has wrong dimension');
+            testCase.verifyEqual(testCase.data.dataTable{:, 1}, ["New data 1"; "Sim 2"; "Sim 3"], 'addData method not working');
+            testCase.data.addData(testCase.datasets{2, :});
+            testCase.verifySize(testCase.data.dataTable, [4, 4], 'data has wrong dimension');
+            testCase.verifyEqual(testCase.data.dataTable{:, 1}, ["New data 1"; "Sim 2"; "Sim 3"; "Bilayer / SMW"], 'addData method not working');
+            testCase.verifyError(@() testCase.data.addData('Sim 2'), duplicateName.errorID);  % duplicates not allowed
+            testCase.verifyError(@() testCase.data.addData(1), invalidType.errorID);  % name should be string
+            testCase.verifyError(@() testCase.data.addData('Another Sim', 'data'), invalidType.errorID);  % data should not string
+            testCase.verifyError(@() testCase.data.addData('Another Sim', zeros(4, 3), [-1, 1]), invalidNumberOfInputs.errorID); % argument should be 1, 2 or 4
+            testCase.verifyError(@() testCase.data.addData('Another Sim', zeros(4, 3), [-1, 1, 6], [-0.5, 0.5]), invalidType.errorID);  % range should be length 2
+            testCase.verifyError(@() testCase.data.addData('Another Sim', zeros(4, 3), [-1, 1], [-0.5, 2, 0.5]), invalidType.errorID);  % range should be length 2
+            testCase.data.addData();
+            testCase.verifySize(testCase.data.dataTable, [5, 4], 'data has wrong dimension');
+            testCase.verifyEqual(testCase.data.dataTable{:, 1}, ["New data 1"; "Sim 2"; "Sim 3"; "Bilayer / SMW"; "New data 5"], 'addData method not working');
+            % appendNewRow changes the ranges to match the data and throws warning
+            testCase.verifyWarning(@() testCase.data.addData('Another Sim', zeros(4, 3), [-1, 1], [0, 0]), '');  
+            testCase.verifyEqual(testCase.data.dataTable{end, 3}, {[0, 0]}, 'addData method not working');
+            testCase.verifyWarning(@() testCase.data.addData('d2', zeros(4, 3), [0, 0], [1, -1]), '');
+            testCase.verifyEqual(testCase.data.dataTable{end, 4}, {[0, 0]}, 'addData method not working');
+            testCase.verifyWarning(@() testCase.data.addData('d3', zeros(4, 3), [1, -1], []), '');
+            testCase.verifyEqual(testCase.data.dataTable{end, 3}, {[0, 0]}, 'addData method not working');    
+        end
+        
+        function testRemoveData(testCase)
+            % Checks that data can be removed
+            testCase.addDataSets();
+            testCase.data.removeData(3);
+            testCase.verifyEqual(testCase.data.dataTable{:, 1}, ["New data 1"; "Bilayer / D2O"; "Bilayer / H2O"], "removeData does not work correctly");
+            % Test removing multiple rows
+            testCase.data.removeData([1 3]);
+            testCase.verifyEqual(testCase.data.dataTable{:, 1}, ["Bilayer / D2O"], "removeData does not work correctly");
+        end
+
+        function testSetdata(testCase)
+            % Checks that data can be modified
+            testCase.addDataSets();
+            names = testCase.data.setData(1, 'name', 'Sim 1', 'data', zeros(4, 3), 'dataRange', [0, 1],'simRange', [1, 2]);
+            expected = {"Sim 1", {zeros(4, 3)}, {[0, 1]}, {[1, 2]}};
+            for i = 1:4
+                testCase.verifyEqual(testCase.data.dataTable{1, i}, expected{i}, 'setData method not working');
+            end
+            testCase.verifyEqual(names.oldName, "New data 1", 'setData returned incorrect data');
+            testCase.verifyEqual(names.newName, 'Sim 1', 'setData returned incorrect data');
+            names = testCase.data.setData('Bilayer / SMW', 'name', 'Sim 2', 'data', ones(4, 3), 'dataRange', [-1, 1],'simRange', [1, 3]);
+            expected = {"Sim 2", {ones(4, 3)}, {[-1, 1]}, {[1, 3]}};
+            for i = 1:4
+                testCase.verifyEqual(testCase.data.dataTable{3, i}, expected{i}, 'setData method not working');
+            end
+            testCase.verifyEqual(names.oldName, string(testCase.datasets{2, 1}), 'setData returned incorrect data');
+            testCase.verifyEqual(names.newName, 'Sim 2', 'setData returned incorrect data');
+            testCase.verifyError(@() testCase.data.setData(1, 'name'), invalidNumberOfInputs.errorID);
+            testCase.verifyError(@() testCase.data.setData('5', 'name', 'data_name'), nameNotRecognised.errorID);
+            testCase.verifyError(@() testCase.data.setData(5, 'name', 'data_name'), indexOutOfRange.errorID);
+            testCase.verifyError(@() testCase.data.setDataName(1, 56), invalidType.errorID);
+            testCase.verifyError(@() testCase.data.setDataName(1, 'Sim 2'), duplicateName.errorID);
+            names = testCase.data.setDataName(1, 'Sim 3');
+            testCase.verifyEqual(testCase.data.dataTable{1, 1}, "Sim 3", 'setDataName method not working');
+            testCase.verifyEqual(names.oldName, "Sim 1", 'setData returned incorrect data');
+            testCase.verifyEqual(names.newName, 'Sim 3', 'setData returned incorrect data');
+         end
+
+        function testDisplayTable(testCase)
+            import matlab.unittest.constraints.ContainsSubstring
+            % Check that the content of the data table are printed
+            actualHeader = {'Name', 'Data', 'Data Range', 'Simulation Range'};
+            
+            display = evalc('testCase.data.displayDataObject()');
+            testCase.verifyNotEmpty(display, 'displayDataObject method not working');
+            
+            displayArray = textscan(display,'%s','Delimiter','\r','TextType','string');
+            displayArray = strip(displayArray{1});
+            % display table should be height of data table plus header and divider row
+            testCase.verifyLength(displayArray, height(testCase.data.dataTable) + 3);
+            % Remove html tags used to format header then split table when
+            % 2 or more spaces are found to avoid splitting names with single space
+            displayHeader = eraseBetween(displayArray{2}, '<', '>', 'Boundaries','inclusive');
+            displayHeader = regexp(displayHeader, '\s{2,}', 'split');
+            testCase.verifyEqual(displayHeader, actualHeader);
+            actualRow = {"New data 1", "No Data", "-", "[ 0.0050 , 0.7000 ]"};
+	        row = regexp(displayArray{4}, '\s{2,}', 'split');
+            row = string(replace(row, '"', ''));
+            testCase.verifyLength(row, length(actualHeader));
+            testCase.verifyEqual(row, string(actualRow), 'displayDataObject method not working')
+                   
+            testCase.data.dataTable(2, :) = {testCase.datasets(2, 1), testCase.datasets(2, 2), testCase.datasets(2, 3), []};
+            display = evalc('testCase.data.displayDataObject()');
+            displayArray = textscan(display,'%s','Delimiter','\r','TextType','string');
+            displayArray = strip(displayArray{1});
+            testCase.verifyLength(displayArray, height(testCase.data.dataTable) + 3);
+            displayHeader = eraseBetween(displayArray{2}, '<', '>', 'Boundaries','inclusive');
+            displayHeader = regexp(displayHeader, '\s{2,}', 'split');
+            testCase.verifyEqual(displayHeader, actualHeader);
+            actualRow = {"Bilayer / SMW", "Data array: [3 x 3]", "[ -1.0000 , 1.0000 ]", "-"};
+	        row = regexp(displayArray{5}, '\s{2,}', 'split');
+            row = string(replace(row, '"', ''));
+            testCase.verifyLength(row, length(actualHeader));
+            testCase.verifyEqual(row, string(actualRow), 'displayDataObject method not working');
+        end
+    end
+end

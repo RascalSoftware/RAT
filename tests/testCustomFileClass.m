5349bae6ae64f6104ec9690cb1e1faa369a20ad6
PaulSharp <44529197+DrPaulSharp@users.noreply.github.com>
2023-02-09 15:55:38 +0000

    Added test "testAddFileInvalid"

--- tests/testCustomFileClass.m
+++ tests/testCustomFileClass.m
@@ -96,6 +96,34 @@ classdef testCustomFileClass < matlab.unittest.TestCase
             testCase.verifyEqual(testCase.exampleClass.fileTable, expectedTable, "addFile does not work correctly");
         end
 
+        function testAddFileEmpty(testCase)
+            % Test adding a custom file entry to the custom file class.
+            % We can add a custom file entry with no parameters, just a
+            % custom object name, a custom object name with the filename,
+            % and a fully defined custom file entry consisting of: a
+            % custom object name, a filename, language and file path.
+            newRow = {'New custom file 1', '', 'Octave', 'pwd'};
+            expectedTable = [testCase.exampleClass.fileTable; newRow];
+
+            testCase.exampleClass.addFile();
+
+            testCase.verifyEqual(testCase.exampleClass.fileTable, expectedTable, "addFile does not work correctly");
+        end
+
+        function testAddFileInvalid(testCase)
+            % Test adding a custom file entry to the custom file class.
+            % If we use an invalid set of custom file parameters it should
+            % raise an error
+
+            % Invalid length for custom file parameters
+            testCase.verifyError(@() testCase.exampleClass.addFile({}), ?MException)
+            testCase.verifyError(@() testCase.exampleClass.addFile({'Invalid Entry', 'matlab', 'pwd'}), ?MException)
+            testCase.verifyError(@() testCase.exampleClass.addFile({'Invalid Entry', 'invalid.m', 'matlab', 'pwd', 'other'}), ?MException)
+
+            % Duplicate custom object names
+            testCase.verifyError(@() testCase.exampleClass.addFile({'DPPC Model'}), ?MException)        
+        end
+
     end
 
 end

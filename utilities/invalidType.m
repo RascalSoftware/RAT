30d156ee4dbdcafd90dccafc90af308ac96d5bf7
PaulSharp <44529197+DrPaulSharp@users.noreply.github.com>
2023-02-13 12:24:53 +0000

    Added arguments block for validation and default error message to "invalidType"

--- utilities/invalidType.m
+++ utilities/invalidType.m
@@ -1,6 +1,9 @@
 classdef invalidType < MException
     methods
         function obj = invalidType(message)
+            arguments
+                message {mustBeTextScalar} = "The given value is not a valid type for this variable";
+            end
             obj = obj@MException('RAT:InvalidType', message);
         end
     end

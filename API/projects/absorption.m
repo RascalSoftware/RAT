ebfe3d459dedcc4ab315c1e02693188443f26f06
PaulSharp <44529197+DrPaulSharp@users.noreply.github.com>
2023-03-23 17:39:34 +0100

    Added new project "absorption.m"

---
+++ API/projects/absorption.m
@@ -0,0 +1,27 @@
+classdef absorption < projectClass
+    
+    methods
+
+        function obj = absorption(experimentName)
+            % Creates a project object for non polarised neutrons
+            % including absoprtion.
+            % The only argument is the experiment name which is a char
+            % array, which is optional
+            %
+            % problem = absorption('New experiment');
+            arguments
+                experimentName {mustBeTextScalar} = ''
+            end
+
+            % Call projectClass constructor
+            obj@projectClass(experimentName);
+
+            % Set values for non polarised neutrons
+            obj.calculationType = calculationTypes.Absorption.value;
+            obj.layers = layersClass({'SLD Real', 'SLD Imaginary'});
+
+        end
+        
+    end
+
+end

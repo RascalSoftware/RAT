ce716ddcd6d2b2b373b031a8cec2e472efa7d592
RabiyaF <rabiya.farooq.226@gmail.com>
2023-04-03 02:29:23 +0100

    refactors projectClassToR1

--- utilities/misc/projectClassToR1.m
+++ utilities/misc/projectClassToR1.m
@@ -6,343 +6,186 @@ function newR1Problem = projectClassToR1(r2Problem, varargin)
 % Now work through updating the values in the r1 struct with equivalent
 % values in the r2 projectClass
 
-
-%% Work out inputs and create directories if necessary
 if ~isempty(varargin)
     pars = parseR1Problem(varargin{:});
-    
-    % Use the named r1Problem
-    r1Problem = pars.r1Problem;
-    if isfield(r1Problem,'empty')                % No r1Problem supplied
-        r1Problem = load('defaultProject.mat');
-        r1Problem = r1Problem.problem;
-    end
-    
     saveTheProject = pars.saveProject;
     fileName = pars.fileName;
-    
-    % (1) Check the directory exists
-    % Check if the required directory exists....
     dirName = pars.dirName;
     dirPath = pars.dirPath;
     fullName = fullfile(dirPath,dirName);
-    originalDir = pwd;
+
+    r1Problem = pars.r1Problem;
+    if isfield(r1Problem,'empty')
+        % load default project if r1Problem not provided
+        r1Problem = load('defaultProject.mat').problem;
+    end
+
     if ~exist(fullName,'dir') && saveTheProject
-        % Directory does not exist
-        % Go to the specified path
-        % and make the project directories.
+        % Create directory if it does not exist
         cd(dirPath);
         mkdir(dirName);
         cd(dirName);
-        originalDir = pwd;
         if ~exist('datafiles','dir')
             mkdir('datafiles');
         end
     end
 end
 
+% Set name, type and experiment_type
+r1Problem.name = r2Problem.experimentName;
+r1Problem.module.type = r2Problem.modelType;
+r1Problem.module.experiment_type = r2Problem.geometry;
 
+% Set parameter names, values, limits and check boxes
+r2ParamStruct = r2Problem.parameters.toStruct();
+r1Problem.params = r2ParamStruct.params;
+r1Problem.paramnames = r2ParamStruct.paramNames;
+r1Problem.constr = vertcat(r2ParamStruct.paramConstr{:});
+r1Problem.fityesno = r2ParamStruct.fitYesNo;
 
-
-%% Geometry and exp type block
-% Get parameters from R2
-geometry = r2Problem.geometry;
-type = r2Problem.modelType;
-name = r2Problem.experimentName;
-
-% Put these into the R1 struct..
-r1Problem.name = name;
-r1Problem.module.type = type;
-r1Problem.module.experiment_type = geometry;
-
-%% Parameter Names, values, limits and check boxes
-r2ParamStruct = r2Problem.parameters.toStruct;
-
-paramNames = r2ParamStruct.paramNames;
-nParams = r2ParamStruct.nParams;
-paramConstr = r2ParamStruct.paramConstr;
-fitYesNo = r2ParamStruct.fitYesNo;
-paramValues = r2ParamStruct.params;
-
-r1Problem.params = paramValues;
-r1Problem.paramnames = paramNames;
-
-theseConstr = [];
-for i = 1:nParams
-    theseConstr(i,:) = paramConstr{i};
-end
-
-r1Problem.constr = theseConstr;
-r1Problem.fityesno = fitYesNo;
-
-%% Qz shifts
-
-
-
-r1Problem.numberOfShifts = 1;
+% Set qz shifts
+[r1Problem.numberOfShifts, r1Problem.numberOfShifts] = deal(1);
 r1Problem.shiftsNames = {'Shift 1'};
-r1Problem.shifts_horisontal = 0;
-r1Problem.numberOfShifts = 1;
+[r1Problem.shifts_horisontal, r1Problem.shifts_fityesno] = deal(0);
 r1Problem.shifts_constr = [-1e-4 1e-4];
-r1Problem.shifts_fityesno = 0;
 
-%% Scalefactors....
+% Set scalefactors
 r2ScalesStruct = r2Problem.scalefactors.toStruct();
+r1Problem.scalesNames = r2ScalesStruct.paramNames;
+r1Problem.scalefac = r2ScalesStruct.params;
+r1Problem.numberOfScales = r2ScalesStruct.nParams;
+r1Problem.scale_constr = vertcat(r2ScalesStruct.paramConstr{:});
+r1Problem.scalefac_fityesno = r2ScalesStruct.fitYesNo;
 
-scalesNames = r2ScalesStruct.paramNames;
-nScales = r2ScalesStruct.nParams;
-scalesConstr = r2ScalesStruct.paramConstr;
-fitYesNo = r2ScalesStruct.fitYesNo;
-scalesValues = r2ScalesStruct.params;
-
-r1Problem.scalesNames = scalesNames;
-r1Problem.scalefac = scalesValues;
-r1Problem.numberOfScales = nScales;
-
-theseConstr = [];
-for i = 1:nScales
-    theseConstr(i,:) = scalesConstr{i};
-end
-
-r1Problem.scale_constr = theseConstr;
-r1Problem.scalefac_fityesno = fitYesNo;
-
-%% Bulk in
+% Set bulk in
 r2BulkInStruct = r2Problem.bulkIn.toStruct();
+r1Problem.nbaNames = r2BulkInStruct.paramNames;
+r1Problem.nba = r2BulkInStruct.params;
+r1Problem.numberOfNbas = r2BulkInStruct.nParams;
+r1Problem.nbairs_constr = vertcat(r2BulkInStruct.paramConstr{:});
+r1Problem.nbairs_fityesno = r2BulkInStruct.fitYesNo;
 
-bulkInNames = r2BulkInStruct.paramNames;
-nbulkIn = r2BulkInStruct.nParams;
-bulkInConstr = r2BulkInStruct.paramConstr;
-bulkInFitYesNo = r2BulkInStruct.fitYesNo;
-bulkInValues = r2BulkInStruct.params;
-
-r1Problem.nbaNames = bulkInNames;
-r1Problem.nba = bulkInValues;
-r1Problem.numberOfNbas = nbulkIn;
-
-theseConstr = [];
-for i = 1:nbulkIn
-    theseConstr(i,:) = bulkInConstr{i};
-end
-
-r1Problem.nbairs_constr = theseConstr;
-r1Problem.nbairs_fityesno = bulkInFitYesNo;
-
-
-%% Bulk out
+% Set bulk out
 r2BulkOutStruct = r2Problem.bulkOut.toStruct();
+r1Problem.nbsNames = r2BulkOutStruct.paramNames;
+r1Problem.nbs = r2BulkOutStruct.params;
+r1Problem.numberOfNbss = r2BulkOutStruct.nParams;
+r1Problem.nbsubs_constr = vertcat(r2BulkOutStruct.paramConstr{:});
+r1Problem.nbsubs_fityesno = r2BulkOutStruct.fitYesNo;
 
-bulkOutNames = r2BulkOutStruct.paramNames;
-nbulkOut = r2BulkOutStruct.nParams;
-bulkOutConstr = r2BulkOutStruct.paramConstr;
-bulkOutFitYesNo = r2BulkOutStruct.fitYesNo;
-bulkOutValues = r2BulkOutStruct.params;
-
-r1Problem.nbsNames = bulkOutNames;
-r1Problem.nbs = bulkOutValues;
-r1Problem.numberOfNbss = nbulkOut;
-
-theseConstr = [];
-for i = 1:nScales
-    theseConstr(i,:) = bulkOutConstr{i};
-end
-
-r1Problem.nbsubs_constr = theseConstr;
-r1Problem.nbsubs_fityesno = bulkOutFitYesNo;
-
-%% Resolutions
+% Set resolutions (only use resolpars - more advanced resolutions
+% unavaliable in R1)
 r2ResolStruct = r2Problem.resolution.toStruct();
+r1Problem.resolNames = r2ResolStruct.resolParNames;
+r1Problem.resolution = r2ResolStruct.resolPars;
+r1Problem.numberOfResolutions = r2ResolStruct.nResolPars;
+r1Problem.reslution_constr = vertcat(r2ResolStruct.resolParConstr{:});
+r1Problem.resolution_fityesno = r2ResolStruct.resolFitYesNo;
 
-% Only use resolpars - don't have more advanced resolutions in R1...
-resolNames = r2ResolStruct.resolParNames;
-nResols = r2ResolStruct.nResolPars;
-resolConstr = r2ResolStruct.resolParConstr;
-resolFitYesNo = r2ResolStruct.resolFitYesNo;
-resolValues = r2ResolStruct.resolPars;
-
-r1Problem.resolNames = resolNames;
-r1Problem.resolution = resolValues;
-r1Problem.numberOfResolutions = nResols;
-
-theseConstr = [];
-for i = 1:nResols
-    theseConstr(i,:) = resolConstr{i};
-end
-
-r1Problem.reslution_constr = theseConstr;
-r1Problem.resolution_fityesno = resolFitYesNo;
-
-
-%% Backgrounds...
+% Set Backgrounds
 r2BackStruct = r2Problem.background.toStruct();
+r1Problem.backsNames = r2BackStruct.backParNames;
+r1Problem.backs = r2BackStruct.backParVals;
+r1Problem.numberOfBacks = r2BackStruct.nBackPars;
+r1Problem.backs_constr = vertcat(r2BackStruct.backParConstr{:});
+r1Problem.backgrounds_fityesno = r2BackStruct.backParFitYesNo;
 
-% Only use resolpars - don't have more advanced resolutions in R1...
-backsNames = r2BackStruct.backParNames;
-nBacks = r2BackStruct.nBackPars;
-backsConstr = r2BackStruct.backParConstr;
-backsFitYesNo = r2BackStruct.backParFitYesNo;
-backsValues = r2BackStruct.backParVals;
-
-r1Problem.backsNames = backsNames;
-r1Problem.backs = backsValues;
-r1Problem.numberOfBacks = nBacks;
-
-theseConstr = [];
-for i = 1:nBacks
-    theseConstr(i,:) = backsConstr{i};
-end
-
-r1Problem.backs_constr = theseConstr;
-r1Problem.backgrounds_fityesno = backsFitYesNo;
-
-
-%% Now, if it is Standard Layers, deal with the layers....
-if strcmpi(type, modelTypes.StandardLayers.value)
-    
-    allLayers = r2Problem.layers;
-    layersTable = table2cell(allLayers.layersTable);
+if strcmpi(r2Problem.modelType, modelTypes.StandardLayers.value)
+    % Set layers (if modelType is standard layers)
+    layersTable = table2cell(r2Problem.layers.layersTable);
     numberOfLayers = size(layersTable,1);
     
     for i = 1:numberOfLayers
         thisLayer = layersTable(i,:);
-        
         thisLayerName = char(thisLayer{1});
-        thisLayerThick = char(thisLayer{2});
-        thisLayerSLD = char(thisLayer{3});
-        thisLayerRough = char(thisLayer{4});
-        thisLayerHydr = char(thisLayer{5});
+        thisThickInd = find(strcmpi(char(thisLayer{2}), r1Problem.paramnames));
+        thisSLDInd = find(strcmpi(char(thisLayer{3}), r1Problem.paramnames));
+        thisRoughInd = find(strcmpi(char(thisLayer{4}), r1Problem.paramnames));
+        thisHydrInd = find(strcmpi(char(thisLayer{5}), r1Problem.paramnames));
         thisLayerHydrWhat = char(thisLayer{6});
-        
-        thisThickInd = find(strcmpi(thisLayerThick,r1Problem.paramnames));
-        thisSLDInd = find(strcmpi(thisLayerSLD,r1Problem.paramnames));
-        thisRoughInd = find(strcmpi(thisLayerRough,r1Problem.paramnames));
-        thisHydrInd = find(strcmpi(thisLayerHydr,r1Problem.paramnames));
-        
+
         if strcmpi(thisLayerHydrWhat, hydrationTypes.BulkOut.value)
             hydrWhat = hydrationTypes.BulkOut.value;
         else
             hydrWhat = hydrationTypes.BulkIn.value;
         end
         
-        newLayerCell = {thisThickInd,thisSLDInd,thisRoughInd,thisHydrInd,thisLayerName,hydrWhat};
-        newLayersDetails{i} = newLayerCell;
+        newLayersDetails{i} = {thisThickInd, thisSLDInd, thisRoughInd, thisHydrInd, thisLayerName, hydrWhat};
         layersNames{i} = thisLayerName;
     end
-
 else
-    % If it's not layers, then set up the relevant custom model..
+    % Set up custom model (if modelType not standard layers)
     newLayersDetails = {};
     numberOfLayers = 0;
     
     % Get the custom file name..
     customFiles = r2Problem.customFile.toStruct().files{:};
-    customFiles = customFiles(1,:); % Only ever 1 custom file in R1
+    customFiles = customFiles(1, :); % Only ever 1 custom file in R1
     customFileName = customFiles{1};
+    originalDir = pwd;
     
     cd(originalDir);
-    copyfile(customFileName,dirName);
+    copyfile(customFileName, dirName);
     cd(dirName);
-    
-    
 end
 
-%% Deal with the data and contrasts
-dataClass = r2Problem.data;         % Data class object
-contrasts = r2Problem.contrasts;    % Contrasts class object
-
-numberOfContrasts = contrasts.numberOfContrasts;
+% Set contrasts           
+numberOfContrasts = r2Problem.contrasts.numberOfContrasts;
 r1Problem.numberOfContrasts = numberOfContrasts;
 
-contrastBacks = zeros(numberOfContrasts,1);
-contrastScales = zeros(numberOfContrasts,1);
-contrastShifts = ones(numberOfContrasts,1);
-contrastResolutions = zeros(numberOfContrasts,1);
-contrastNbas = zeros(numberOfContrasts,1);
-contrastNbss = zeros(numberOfContrasts,1);
-contrastLayers = cell(numberOfContrasts,1);
-contrastsNumberOfLayers = zeros(numberOfContrasts,1);
-forceReload = zeros(numberOfContrasts,1);
-
-simLimits = cell(numberOfContrasts,1);
-dataLimits = cell(numberOfContrasts,1);
-fitlowrange = zeros(numberOfContrasts,1);
-fithirange = zeros(numberOfContrasts,1);
-
-data = cell(numberOfContrasts,1);
-contrastNames = cell(numberOfContrasts,1);
-contrastFiles = cell(numberOfContrasts,1);
-dataPath = {};
+[contrastBacks, contrastScales, contrastResolutions, contrastNbas, contrastNbss, contrastsNumberOfLayers, forceReload, fitlowrange, fithirange] = deal(zeros(numberOfContrasts, 1));
+contrastShifts = ones(numberOfContrasts, 1);
+[contrastLayers, simLimits, dataLimits, data, contrastNames, contrastFiles] = deal(cell(numberOfContrasts, 1));
 
 for i = 1:numberOfContrasts
-    thisContrast = contrasts.contrasts{i};
-    
-    % The the relative components
-    thisName = thisContrast.name;
-    thisNba = thisContrast.nba;
-    thisNbs = thisContrast.nbs;
-    thisResol = thisContrast.resolution;
-    thisScale = thisContrast.scalefactor;
-    thisBack = thisContrast.background;
-    
-    % Get the relevant data from the r2 project
-    contrastNames{i} = thisName;
-    
-    % Nba..
-    bulkInLoc = strfind(bulkInNames,thisNba);
-    bulkInIndex = find(not(cellfun('isempty',bulkInLoc)));
-    contrastNbas(i) = bulkInIndex;
-    
-    % Nbs
-    bulkOutLoc = strfind(bulkOutNames,thisNbs);
-    bulkOutIndex = find(not(cellfun('isempty',bulkOutLoc)));
-    contrastNbss(i) = bulkOutIndex;
-    
-    % Scalefactors
-    scaleLoc = strfind(scalesNames,thisScale);
-    scaleIndex = find(not(cellfun('isempty',scaleLoc)));
-    contrastScales(i) = scaleIndex;
-   
-    % Resol
-    % Find the resol name in the resolutions struct
-    resolutionNames = r2ResolStruct.resolutionNames;
-    thisResolInd = find(strcmp(resolutionNames,thisResol));
-    
-    % Find which resolution par this is..
-    thisResolParName = r2ResolStruct.resolutionValues{thisResolInd,1};
-    resolParNames = r2ResolStruct.resolParNames;    
-    thisResolParInd = find(strcmp(resolParNames,thisResolParName));
-    contrastResolutions(i) = thisResolParInd;
-    
-    % Backgrounds
-    % Find the Background name in the backgrounds struct
-    backgroundNames = r2BackStruct.backgroundNames;
-    thisBacksInd = find(strcmp(backgroundNames,thisBack));
-    
-    % Find which resolution par this is..
-    thisBacksParName = r2BackStruct.backgroundValues{thisBacksInd,1};
-    backParNames = r2BackStruct.backParNames;    
-    thisBackParInd = find(strcmp(backParNames,thisBacksParName));
-    contrastBacks(i) = thisBackParInd;
 
-    % Data
-    dataTable = table2cell(dataClass.dataTable);
+    thisContrast = r2Problem.contrasts.contrasts{i};
+    
+    contrastNames{i} = thisContrast.name;
+    
+    % nba
+    bulkInLoc = strfind(r2BulkInStruct.paramNames, thisContrast.nba);
+    contrastNbas(i) = find(not(cellfun('isempty', bulkInLoc)));
+    
+    % nbs
+    bulkOutLoc = strfind(r2BulkOutStruct.paramNames, thisContrast.nbs);
+    contrastNbss(i) = find(not(cellfun('isempty', bulkOutLoc)));
+    
+    % scalefactors
+    scaleLoc = strfind(r1Problem.scalesNames, thisContrast.scalefactor);
+    contrastScales(i) = find(not(cellfun('isempty', scaleLoc)));
+   
+    % resol (find the resol name in the resolutions struct)
+    resolutionNames = r2ResolStruct.resolutionNames;
+    thisResolInd = find(strcmp(resolutionNames, thisContrast.resolution));
+    
+    % find which resolution par this is..
+    thisResolParName = r2ResolStruct.resolutionValues{thisResolInd, 1}; 
+    contrastResolutions(i) = find(strcmp(r2ResolStruct.resolParNames, thisResolParName));
+    
+    % backgrounds (find the background name in the backgrounds struct)
+    backgroundNames = r2BackStruct.backgroundNames;
+    thisBacksInd = find(strcmp(backgroundNames, thisContrast.background));
+    
+    % find which backs par this is..
+    thisBacksParName = r2BackStruct.backgroundValues{thisBacksInd, 1};    
+    contrastBacks(i) = find(strcmp(r2BackStruct.backParNames, thisBacksParName));
+
+    % data
+    dataTable = table2cell(r2Problem.data.dataTable);
     r2DataNames = dataTable(:,1);
     r2DataNames = cellfun(@(x)char(x), r2DataNames, 'UniformOutput', false);
     thisContrastData = thisContrast.data;
     thisContrastLocation = find(strcmp(r2DataNames,thisContrastData));
-    thisData = dataTable{thisContrastLocation,2};
-    thisSimLimits = dataTable{thisContrastLocation,4};
-    thisDataLimits = dataTable{thisContrastLocation,3};
-    data{i} = thisData;
-    simLimits{i} = thisSimLimits;
-    dataLimits{i} = thisDataLimits;
-    
-    fitlowrange(i) = thisDataLimits(1);
-    fithirange(i) = thisDataLimits(2);
+    data{i} = dataTable{thisContrastLocation,2};
+    simLimits{i} = dataTable{thisContrastLocation,4};
+    dataLimits{i} = dataTable{thisContrastLocation,3};
+    fitlowrange(i) = dataTable{thisContrastLocation,3}(1);
+    fithirange(i) = dataTable{thisContrastLocation,3}(2);
     
     if ~strcmpi(thisContrastData,'simulation')
         dataPresent(i) = 1;
-        include_data(i) = 1;
+        include_data(i)= 1;
         dataTypes{i} = 'Ascii File';
     else
         dataPresent(i) = 0;
@@ -350,40 +193,37 @@ for i = 1:numberOfContrasts
         dataTypes{i} = 'Simulation';
     end
     
-    % Now deal with the model. Depends on whether it is standard layers or
-    % a custom model.
-    if strcmpi(type,modelTypes.StandardLayers.value)
+    if strcmpi(r2Problem.modelType, modelTypes.StandardLayers.value)
+        % If modelType is StandardLayers
         % Set up the contrastLayers strings
         % Get the model for this contrast
         model = thisContrast.model;
+        contrastsNumberOfLayers = length(model);
         thisLayerString = '';
-        for n = 1:length(model)
+        for n = 1:contrastsNumberOfLayers
             thisLay = model{n};
-            thisLayNum = find(strcmpi(layersNames,thisLay));
+            thisLayNum = find(strcmpi(layersNames, thisLay));
             thisLayChar = sprintf('%s,',num2str(thisLayNum));
             thisLayerString = [thisLayerString thisLayChar];
         end
         contrastLayers{i} = thisLayerString;
-        contrastsNumberOfLayers = length(model);
         customFileName = '';
     else
-        % We are using a custom model
+        % % If modelType is NOT StandardLayers (custom model)
         whichCustom = thisContrast.model{:};
         fileTable = table2cell(r2Problem.customFile.fileTable);
         customNames = fileTable(:,1);
         customNames = cellfun(@(x)char(x), customNames, 'UniformOutput', false);
         thisCustomLocation = find(strcmp(customNames,whichCustom));
-        customFileName = char(fileTable{thisCustomLocation,2});
+        customFileName = char(fileTable{thisCustomLocation, 2});
         contrastLayers{i} = '';
         contrastsNumberOfLayers(i) = 0;
     end
     contrastRepeatSLDs{i} = [0 1];
 end
 
+% Put everything into the R1 problem
 r1Problem.contrastTypes = dataTypes;
-
-%%
-%r1Problem.module.name = customFileName;
 r1Problem.contrastNames = contrastNames;
 r1Problem.contrastNbas = contrastNbas;
 r1Problem.contrastNbss = contrastNbss;
@@ -396,7 +236,6 @@ r1Problem.include_data = include_data;
 r1Problem.dataPresent = dataPresent;
 r1Problem.contrastLayers = contrastLayers;
 r1Problem.contrastsNumberOfLayers = contrastsNumberOfLayers;
-% Put everything into the R1 problem
 r1Problem.layersDetails = newLayersDetails;
 r1Problem.numberOfLayers = numberOfLayers;    
 r1Problem.module.name = customFileName;
@@ -407,12 +246,10 @@ r1Problem.fitlowrange = fitlowrange;
 r1Problem.fithirange = fithirange;
 r1Problem.repeatLayers = contrastRepeatSLDs;
 
-% Finally need to actually fill in some calculation results in the r1
-% model. We can do this using RAT...
-
+% Set calculation results in the r1 model using RAT
 controls = controlsClass;
 controls.display = displayOptions.Off.value;
-[~,results] = RAT(r2Problem,controls);
+[~,results] = RAT(r2Problem, controls);
 
 r1Problem.shifted_data = results.shifted_data;
 r1Problem.calculations.Simulation = results.Simulation;
@@ -423,12 +260,9 @@ r1Problem.calculations.sum_chi = results.calculationResults.sum_chi;
 
 newR1Problem = r1Problem;
 
-% If the save project flag is set, then save the new peoject
 if saveTheProject
-    
-    % Save and name the datafiles, and set the paths, data filenames in the 
-    % r1problem
-    
+    % save new peoject if the save flag set
+    % Save datafiles, set paths and data filenames
     data = r1Problem.data;
     for i = 1:length(data)
         thisData = data{i};
@@ -483,9 +317,7 @@ function inputBlock = parseR1Problem(varargin)
         actualDiffs = intersect(diffs,allowedDiffs);
         
         if ~isequal(actualDiffs,diffs)
-            ME = MException('RAT:unrecognizedR1Problem', ...
-            'R1Problem does not have right format');
-            throw(ME);
+            throw(unrecognizedR1Problem);
         end
     end
     
@@ -587,7 +419,3 @@ names =     {'path'          ;
     'resample'};
 
 end
-
-
-
-

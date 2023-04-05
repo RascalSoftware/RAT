19aafcde64cc959c739486ccbc28a708bc6d0e66
RabiyaF <rabiya.farooq.226@gmail.com>
2023-04-05 06:38:02 +0100

    added example usage of conversion functions

---
+++ examples/conversion/conversionExamples.m
@@ -0,0 +1,71 @@
+%% Convert r1 struct to projectClass.
+% 
+% The following example shows how to convert r1 structs into projectClass.
+% 
+% R1 struct is converted into a projectClass using r1ToProjectClass
+% 
+% The functions takes the name of the r1 struct .mat file 
+%
+% The function usage:
+
+projClass = r1ToProjectClass("originalDSPCBilayerStructInput.mat");
+
+
+%% Convert projectClass to r1 struct.
+% 
+% The following example shows how to convert projectClass into r1 struct.
+% 
+% projectClass is converted into r1 struct using projectClassToR1
+% 
+% The functions takes in a projectClass and other optional arguments.
+%
+% The optional arguments are:
+%
+% r1Problem (class struct):    The r1 struct which is updated with equivalent 
+%                              projectClass values. 
+%                              [default] defaultProject.mat.
+%
+% dirName (class char):        The directory where the files are saved. 
+%                              [default] newDirectory.
+%
+% dirPath (class char):        The path where the new directory is created. 
+%                              [default] current directory (pwd).
+%
+% saveproject (class logical): A boolean that determines if files are saved. 
+%                              [default] true.
+%
+% fileName (class char):       The filename of the resulting r1 struct. 
+%                              [default] newFile appended with current 
+%                              timestamp.
+%
+% The function usage:
+%
+% First load the projectClass to be converted using the following syntax
+
+pClass = load("originalDSPCBilayerProjectClass.mat").thisProjectClass;
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Conversion Example 1
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+r1Struct = projectClassToR1(pClass, 'saveProject', false);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Conversion Example 2 (uncomment to run)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% r1Struct = projectClassToR1(pClass,...
+%                             'fileName', 'r1Struct',...
+%                             'dirName', 'results');
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Conversion Example 3 (uncomment, fill and run)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% r1Struct = projectClassToR1({projectClass}, ...
+%                             'dirName', {dirName},...
+%                             'saveProject', {true or false}, ... 
+%                             'dirPath', {path},...
+%                             'r1Problem', {r1StructName},...
+%                             'fileName', {fileName});
+

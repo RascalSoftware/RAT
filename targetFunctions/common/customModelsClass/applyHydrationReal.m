86707641485473575311fb40174dad6f9a49b4de
arwel <arwel.hughes@stfc.ac.uk>
2023-06-13 09:15:55 +0100

    Staging commit

--- targetFunctions/common/customModelsClass/applyHydrationReal.m
+++ targetFunctions/common/customModelsClass/applyHydrationReal.m
@@ -1,20 +1,30 @@
-function thisContrastLayers = applyHydrationReal(thisContrastLayers,useImaginary)
+function thisContrastLayers = applyHydrationReal(thisContrastLayers,bulkIn,bulkOut,useImaginary)
 
-% Applies the hydration correction for real value of layers
+% Applies the hydration correction to real value of layers
 % if it is necessary..
 
+% Pick correct column for hydration
+if useImaginary
+    hydrCol = 5;
+else
+    hydrCol = 4;
+end
 
-outSize = size(thisContrastLayers);
-if (useImaginary && outSize(2) == 6) || (~useImaginary && outSize(2) == 5)                                     % we need to calculate the hydrated SLD
+% The only guidance we have to whether the user is using hydration
+% in their custom model is the numbre of columns of the output
+outSize = size(thisContrastLayers);     % [nlayers x nCols] 
+
+if (useImaginary && outSize(2) == 6) || (~useImaginary && outSize(2) == 5)   % we need to calculate the hydrated SLD
+
     newOutLayers = zeros(outSize(1),3);
-    newOutLayers(:,1) = thisContrastLayers(:,1);         % Thickness'
-    newOutLayers(:,3) = thisContrastLayers(:,3);         % Roughness
+    newOutLayers(:,1) = thisContrastLayers(:,1);               % Thickness'
+    newOutLayers(:,3) = thisContrastLayers(:,3);               % Roughness
 
     for n = 1:outSize(1)
         thisSLD = thisContrastLayers(n,2);
-        thisHydration = thisContrastLayers(n,4) / 100;   % Assume percent for backwards compatability
-        thisHydrWhat = thisContrastLayers(n,5);
-        if thisHydrWhat == 0                             % Bulk out
+        thisHydration = thisContrastLayers(n,hydrCol) / 100;   % Assume percent for backwards compatability
+        thisHydrWhat = thisContrastLayers(n,hydrCol + 1);
+        if thisHydrWhat == 0                            
             thisBulkHydr = bulkIn;
         else
             thisBulkHydr = bulkOut;

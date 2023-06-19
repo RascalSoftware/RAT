231e47430580385ce99d04b9b380b732cbf22926
RabiyaF <rabiya.farooq.226@gmail.com>
2023-06-19 22:55:32 +0100

    added the setProcedure() method to controlsClass()

--- API/controlsClass/controlsClass.m
+++ API/controlsClass/controlsClass.m
@@ -215,6 +215,73 @@ classdef controlsClass < matlab.mixin.CustomDisplay
                              strjoin(boundHandlingOptions.values(), ', '));
             obj.boundHandling = validateOption(val, 'boundHandlingOptions', message).value;
         end 
+
+        function obj = setProcedure(obj, procedure, varargin)
+            % Method sets the properties of the class based on the selected procedures.
+            %
+            % obj.setProcedure(procedure, varargin)
+            %
+            % Usage Examples:
+            % obj.setProcedure('simplex',...
+            %                   {'tolX', 1e-6,...
+            %                   'tolFun', 1e-6,...
+            %                   'maxFunEvals', 1000})
+            % obj.setProcedure('dream')
+            % obj.setProcedure('ns',
+            %                  {'Nlive', 150,...
+            %                  'Nmcmc', 0,...
+            %                  'propScale', 0.1,...
+            %                  'nsTolerance', 0.1})
+
+            switch procedure
+
+                case procedures.Calculate.value
+                    % Parses the inputs and sets the object properties of
+                    % the Calculate procedure
+                    if ~isempty(varargin)
+                        obj = obj.processCalculateInput(varargin{:});
+                    end
+                    obj.procedure = procedures.Calculate.value;
+
+                case procedures.Simplex.value
+                    % Parses the inputs and sets the object properties of
+                    % the Simplex procedure
+                    if ~isempty(varargin)
+                        obj = obj.processSimplexInput(varargin{:});
+                    end
+                    obj.procedure = procedures.Simplex.value;
+
+                case procedures.DE.value
+                    % Parses the inputs and sets the object properties of
+                    % the Differential Evolution procedure
+                    if ~isempty(varargin)
+                        obj = obj.processDEInput(varargin{:});
+                    end
+                    obj.procedure = procedures.DE.value;
+
+                case procedures.NS.value
+                    % Parses the inputs and sets the object properties of
+                    % the Nested Sampler procedure
+                    if ~isempty(varargin)
+                        obj = obj.processNSInput(varargin{:});
+                    end
+                    obj.procedure = procedures.NS.value;
+
+                case procedures.Dream.value
+                    % Parses the inputs and sets the object properties of
+                    % the DREAM procedure
+                    if ~isempty(varargin)
+                        obj = obj.processDreamInput(varargin{:});
+                    end
+                    obj.procedure = procedures.Dream.value;
+
+                otherwise
+                    % invalid procedure
+                    throw(invalidValue(sprintf('%s is not a supported procedure. Try calculate, simplex, de, ns or dream.', procedure)));
+
+            end
+
+        end
     end
 
     %------------------------- Display Methods --------------------------
@@ -228,6 +295,8 @@ classdef controlsClass < matlab.mixin.CustomDisplay
                 'tolFun', {obj.tolFun},...
                 'maxFunEvals', {obj.maxFunEvals},...
                 'maxIter', {obj.maxIter},...
+                'updateFreq', {obj.updateFreq},...
+                'updatePlotFreq', {obj.updatePlotFreq},...
                 'populationSize', {obj.populationSize},...
                 'fWeight', {obj.fWeight},...
                 'crossoverProbability', {obj.crossoverProbability},...
@@ -248,7 +317,9 @@ classdef controlsClass < matlab.mixin.CustomDisplay
             simplexCell = {'tolX',...
                 'tolFun',...
                 'maxFunEvals',...
-                'maxIter'};
+                'maxIter',...
+                'updateFreq',...
+                'updatePlotFreq'};
             
             deCell = {'populationSize',...
                 'fWeight',...
@@ -286,5 +357,313 @@ classdef controlsClass < matlab.mixin.CustomDisplay
                 groups = getPropertyGroups@matlab.mixin.CustomDisplay(obj);
             end           
         end
+
+        %------------------------- Parsing Methods --------------------------
+
+        function obj = processCalculateInput(obj, varargin)
+            % Parses calculate keyword/value pairs and sets the properties of the class.
+            %
+            % obj.processCalculateInput('param', 'value')
+            %
+            % The parameters that can be set when using Dream procedure are
+            % 1) parallel
+            % 2) calcSldDuringFit
+            % 3) resamPars
+            % 4) display
+
+            % The default values for Calculate
+            defaultParallel = parallelOptions.Single.value;
+            defaultCalcSldDuringFit = false;
+            defaultResamPars = [0.9 50];
+            defaultDisplay = displayOptions.Iter.value;
+            
+            % Creates the input parser for the calculate parameters
+            p = inputParser;
+            addParameter(p,'parallel',  defaultParallel,   @(x) isText(x) || isenum(x));
+            addParameter(p,'calcSldDuringFit',   defaultCalcSldDuringFit,    @islogical);
+            addParameter(p,'resamPars', defaultResamPars,  @isnumeric);
+            addParameter(p,'display',   defaultDisplay,    @(x) isText(x) || isenum(x));
+            properties = varargin{:};
+
+            % Parses the input or raises invalidParameter error 
+            try
+                parse(p, properties{:});
+                inputBlock = p.Results;
+            catch ME
+                if (strcmp(ME.identifier,'MATLAB:InputParser:UnmatchedParameter'))
+                    throw(invalidParameter('Only parallel, calcSldDuringFit, resamPars and display can be set while using the Calculate procedure'));
+                else
+                    rethrow(ME)
+                end
+            end
+
+            % Sets the values the for Calculate parameters
+            obj.parallel = inputBlock.parallel;
+            obj.calcSldDuringFit = inputBlock.calcSldDuringFit;
+            obj.resamPars = inputBlock.resamPars;
+            obj.display = inputBlock.display;
+        end
+
+        function obj = processSimplexInput(obj, varargin)
+            % Parses simplex keyword/value pairs and sets the properties of the class.
+            %
+            % obj.parseSimplexInput('param', 'value')
+            %
+            % The parameters that can be set when using simplex procedure are 
+            % 1) tolX
+            % 2) tolFun
+            % 3) maxFunEvals
+            % 4) maxIter
+            % 5) updateFreq
+            % 6) updatePlotFreq
+            % 7) parallel
+            % 8) calcSldDuringFit
+            % 9) resamPars
+            % 10) display
+
+            % The simplex default values
+            defaultTolX = 1e-6;
+            defaultTolFun = 1e-6;
+            defaultMaxFunEvals = 10000;
+            defaultMaxIter = 1000;
+            defaultUpdateFreq = -1;
+            defaultUpdatePlotFreq = -1;
+            defaultParallel = parallelOptions.Single.value;
+            defaultCalcSldDuringFit = false;
+            defaultResamPars = [0.9 50];
+            defaultDisplay = displayOptions.Iter.value;
+
+            % Parses the input for simplex parameters
+            p = inputParser;
+            addParameter(p,'tolX',  defaultTolX,   @isnumeric);
+            addParameter(p,'tolFun',   defaultTolFun,    @isnumeric);
+            addParameter(p,'maxFunEvals', defaultMaxFunEvals,  @isnumeric);
+            addParameter(p,'maxIter',   defaultMaxIter,    @isnumeric);
+            addParameter(p,'updateFreq',   defaultUpdateFreq,    @isnumeric);
+            addParameter(p,'updatePlotFreq',   defaultUpdatePlotFreq,    @isnumeric);
+            addParameter(p,'parallel',  defaultParallel,   @(x) isText(x) || isenum(x));
+            addParameter(p,'calcSldDuringFit',   defaultCalcSldDuringFit,    @islogical);
+            addParameter(p,'resamPars', defaultResamPars,  @isnumeric);
+            addParameter(p,'display',   defaultDisplay,    @(x) isText(x) || isenum(x));
+            properties = varargin{:};
+                       
+            % Parses the input or raises invalidParameter error 
+            try
+                parse(p, properties{:});
+                inputBlock = p.Results;
+            catch ME
+                if (strcmp(ME.identifier,'MATLAB:InputParser:UnmatchedParameter'))
+                    throw(invalidParameter('Only tolX, tolFun, maxFunEvals, maxIter, updateFreq, updatePlotFreq, parallel, calcSldDuringFit, resamPars and display can be set while using the Simplex procedure.'));
+                else
+                    rethrow(ME)
+                end
+            end
+
+            % Sets the values the for simplex parameters
+            obj.tolX = inputBlock.tolX;
+            obj.tolFun = inputBlock.tolFun;
+            obj.maxFunEvals = inputBlock.maxFunEvals;
+            obj.maxIter = inputBlock.maxIter;
+            obj.updateFreq = inputBlock.updateFreq;
+            obj.updatePlotFreq = inputBlock.updatePlotFreq;
+            obj.parallel = inputBlock.parallel;
+            obj.calcSldDuringFit = inputBlock.calcSldDuringFit;
+            obj.resamPars = inputBlock.resamPars;
+            obj.display = inputBlock.display;
+        end
+
+        function obj = processDEInput(obj, varargin)
+            % Parses differential evolution keyword/value pairs and sets the properties of the class.
+            %
+            % obj.processDEInput('param', 'value')
+            %
+            % The parameters that can be set when using de procedure are 
+            % 1) populationSize
+            % 2) fWeight
+            % 3) crossoverProbability
+            % 4) strategy
+            % 5) targetValue
+            % 6) numGenerations
+            % 7) parallel
+            % 8) calcSldDuringFit
+            % 9) resamPars
+            % 10) display
+
+            % The default values for DE
+            defaultPopulationSize = 20;
+            defaultFWeight = 0.5;
+            defaultCrossoverProbability = 0.8;
+            defaultStrategy = 4;
+            defaultTargetValue = 1;
+            defaultNumGenerations = 500;
+            defaultParallel = parallelOptions.Single.value;
+            defaultCalcSldDuringFit = false;
+            defaultResamPars = [0.9 50];
+            defaultDisplay = displayOptions.Iter.value;
+
+            % Creates the input parser for the DE parameters
+            p = inputParser;
+            addParameter(p,'populationSize',  defaultPopulationSize,   @isnumeric);
+            addParameter(p,'fWeight',   defaultFWeight,    @isnumeric);
+            addParameter(p,'crossoverProbability', defaultCrossoverProbability,  @isnumeric);
+            addParameter(p,'strategy',   defaultStrategy,    @isnumeric);
+            addParameter(p,'targetValue',   defaultTargetValue,    @isnumeric);
+            addParameter(p,'numGenerations',   defaultNumGenerations,    @isnumeric);
+            addParameter(p,'parallel',  defaultParallel,   @(x) isText(x) || isenum(x));
+            addParameter(p,'calcSldDuringFit',   defaultCalcSldDuringFit,    @islogical);
+            addParameter(p,'resamPars', defaultResamPars,  @isnumeric);
+            addParameter(p,'display',   defaultDisplay,    @(x) isText(x) || isenum(x));
+            properties = varargin{:};
+                       
+            % Parses the input or raises invalidParameter error 
+            try
+                parse(p, properties{:});
+                inputBlock = p.Results;
+            catch ME
+                if (strcmp(ME.identifier,'MATLAB:InputParser:UnmatchedParameter'))
+                    throw(invalidParameter('Only populationSize, fWeight, crossoverProbability, strategy, targetValue, numGenerations, parallel, calcSldDuringFit, resamPars and display can be set while using the Differential Evolution procedure'));
+                else
+                    rethrow(ME)
+                end
+            end
+
+            % Sets the values the for DE parameters
+            obj.populationSize = inputBlock.populationSize;
+            obj.fWeight = inputBlock.fWeight;
+            obj.crossoverProbability = inputBlock.crossoverProbability;
+            obj.strategy = inputBlock.strategy;
+            obj.targetValue = inputBlock.targetValue;
+            obj.numGenerations = inputBlock.numGenerations;
+            obj.parallel = inputBlock.parallel;
+            obj.calcSldDuringFit = inputBlock.calcSldDuringFit;
+            obj.resamPars = inputBlock.resamPars;
+            obj.display = inputBlock.display;
+        end
+
+        function obj = processNSInput(obj, varargin)
+            % Parses nested sampler keyword/value pairs and sets the properties of the class.
+            %
+            % obj.processNSInput('param', 'value')
+            %
+            % The parameters that can be set when using de procedure are 
+            % 1) Nlive
+            % 2) Nmcmc
+            % 3) propScale
+            % 4) nsTolerance
+            % 5) parallel
+            % 6) calcSldDuringFit
+            % 7) resamPars
+            % 8) display
+
+            % The default values for NS
+            defaultNlive = 150;
+            defaultNmcmc = 0;
+            defaultPropScale = 0.1;    
+            defaultNsTolerance = 0.1;
+            defaultParallel = parallelOptions.Single.value;
+            defaultCalcSldDuringFit = false;
+            defaultResamPars = [0.9 50];
+            defaultDisplay = displayOptions.Iter.value;
+
+            % Creates the input parser for the NS parameters
+            p = inputParser;
+            addParameter(p,'Nlive',  defaultNlive,   @isnumeric);
+            addParameter(p,'Nmcmc',   defaultNmcmc,    @isnumeric);
+            addParameter(p,'propScale', defaultPropScale,  @isnumeric);
+            addParameter(p,'nsTolerance',   defaultNsTolerance,    @isnumeric);
+            addParameter(p,'parallel',  defaultParallel,   @(x) isText(x) || isenum(x));
+            addParameter(p,'calcSldDuringFit',   defaultCalcSldDuringFit,    @islogical);
+            addParameter(p,'resamPars', defaultResamPars,  @isnumeric);
+            addParameter(p,'display',   defaultDisplay,    @(x) isText(x) || isenum(x));
+            properties = varargin{:};
+            
+            % Parses the input or raises invalidParameter error
+            try
+                parse(p, properties{:});
+                inputBlock = p.Results;
+            catch ME
+                if (strcmp(ME.identifier,'MATLAB:InputParser:UnmatchedParameter'))
+                    throw(invalidParameter('Only Nlive, Nmcmc, propScale, nsTolerance, parallel, calcSldDuringFit, resamPars and display can be set while using the Nested Sampler procedure'));
+                else
+                    rethrow(ME)
+                end
+            end
+
+            % Sets the values the for NS parameters
+            obj.Nlive = inputBlock.Nlive;
+            obj.Nmcmc = inputBlock.Nmcmc;
+            obj.propScale = inputBlock.propScale;
+            obj.nsTolerance = inputBlock.nsTolerance;
+            obj.parallel = inputBlock.parallel;
+            obj.calcSldDuringFit = inputBlock.calcSldDuringFit;
+            obj.resamPars = inputBlock.resamPars;
+            obj.display = inputBlock.display;
+        end
+
+        function obj = processDreamInput(obj, varargin)
+            % Parses Dream keyword/value pairs and sets the properties of the class.
+            %
+            % obj.processDreamInput('param', 'value')
+            %
+            % The parameters that can be set when using Dream procedure are
+            % 1) nSamples
+            % 2) nChains
+            % 3) lambda
+            % 4) pUnitGamma
+            % 5) boundHandling
+            % 6) parallel
+            % 7) calcSldDuringFit
+            % 8) resamPars
+            % 9) display
+
+            % The default values for Dream
+            defaultNSamples = 50000;
+            defaultNChains = 10;
+            defaultLambda = 0.5;
+            defaultPUnitGamma = 0.2;
+            defaultBoundHandling = boundHandlingOptions.Fold.value;
+            defaultParallel = parallelOptions.Single.value;
+            defaultCalcSldDuringFit = false;
+            defaultResamPars = [0.9 50];
+            defaultDisplay = displayOptions.Iter.value;
+
+            % Creates the input parser for the Dream parameters
+            p = inputParser;
+            addParameter(p,'nSamples',  defaultNSamples,   @isnumeric);
+            addParameter(p,'nChains',   defaultNChains,    @isnumeric);
+            addParameter(p,'lambda', defaultLambda,  @isnumeric);
+            addParameter(p,'pUnitGamma',   defaultPUnitGamma,    @isnumeric);
+            addParameter(p,'boundHandling',   defaultBoundHandling, @(x) isText(x) || isenum(x));
+            addParameter(p,'parallel',  defaultParallel,   @(x) isText(x) || isenum(x));
+            addParameter(p,'calcSldDuringFit',   defaultCalcSldDuringFit,    @islogical);
+            addParameter(p,'resamPars', defaultResamPars,  @isnumeric);
+            addParameter(p,'display',   defaultDisplay,    @(x) isText(x) || isenum(x));
+            properties = varargin{:};
+
+            % Parses the input or raises invalidParameter error 
+            try
+                parse(p, properties{:});
+                inputBlock = p.Results;
+            catch ME
+                if (strcmp(ME.identifier,'MATLAB:InputParser:UnmatchedParameter'))
+                    throw(invalidParameter('Only nSamples, nChains, lambda, pUnitGamma, boundHandling, parallel, calcSldDuringFit, resamPars and display can be set while using the DREAM procedure'));
+                else
+                    rethrow(ME)
+                end
+            end
+
+            % Sets the values the for Dream parameters
+            obj.nSamples = inputBlock.nSamples;
+            obj.nChains = inputBlock.nChains;
+            obj.lambda = inputBlock.lambda;
+            obj.pUnitGamma = inputBlock.pUnitGamma;
+            obj.boundHandling = inputBlock.boundHandling;
+            obj.parallel = inputBlock.parallel;
+            obj.calcSldDuringFit = inputBlock.calcSldDuringFit;
+            obj.resamPars = inputBlock.resamPars;
+            obj.display = inputBlock.display;
+        end
+
+      
     end
 end

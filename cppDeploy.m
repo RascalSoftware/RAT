8aabb59ea4ff35cf24001706e8e3dc46193c62f2
Stephen Nneji <steve.nneji@gmail.com>
2024-02-12 12:04:36 +0000

    Adds action to deploy generated cpp code to a separate branch

---
+++ cppDeploy.m
@@ -0,0 +1,14 @@
+% Copies all files required to build cpp to 'cppDeploy' folder
+[~, ~, ~] = rmdir('compile/fullCompile/cppDeploy', 's');
+load compile/fullCompile/codegen/lib/RATMain/buildInfo.mat;
+packNGo(buildInfo,'fileName','deploy.zip');
+unzip('compile/fullCompile/deploy.zip', 'compile/fullCompile/cppDeploy');
+
+% Copy events
+mkdir('compile/fullCompile/cppDeploy/events/');
+copyfile('compile/events/eventManager.cpp', 'compile/fullCompile/cppDeploy/events/eventManager.cpp');
+copyfile('compile/events/eventManager.h', 'compile/fullCompile/cppDeploy/events/eventManager.h');
+copyfile('compile/events/eventManagerImpl.hpp', 'compile/fullCompile/cppDeploy/events/eventManagerImpl.hpp');
+
+% Clean up
+delete 'compile/fullCompile/deploy.zip' 'compile/fullCompile/cppDeploy/buildInfo.mat';

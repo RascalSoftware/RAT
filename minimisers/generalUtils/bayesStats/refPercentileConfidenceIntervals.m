5a9a908c7391ab4ddaef14e0ff40e0553616e022
Paul Sharp <44529197+DrPaulSharp@users.noreply.github.com>
2024-07-29 14:48:52 +0100

    Removes "XData" variables from BayesResults struct

--- minimisers/generalUtils/bayesStats/refPercentileConfidenceIntervals.m
+++ minimisers/generalUtils/bayesStats/refPercentileConfidenceIntervals.m
@@ -191,8 +191,6 @@ end
 
 allPredInts.reflectivity = refErrors;
 allPredInts.sld = sldErrors;
-allPredInts.reflectivityXData = refXVals;
-allPredInts.sldXData = sldXVals;
 allPredInts.sampleChi = sampleChi;
 
 end

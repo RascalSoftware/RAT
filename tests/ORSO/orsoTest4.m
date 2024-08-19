e9cf09a8b0af70415df0fc359e2f2f90d543cffe
Paul Sharp <44529197+DrPaulSharp@users.noreply.github.com>
2024-08-19 17:36:18 +0100

    Moves ORSO validation to tests

--- tests/ORSO/orsoTest4.m
+++ tests/ORSO/orsoTest4.m
@@ -1,36 +1,36 @@
-function out = orsoTest2()
-% ORSO validation Test2
+function out = orsoTest4()
+% ORSO validation Test4 - Reflectivity plus resolution...
 
-layers = dlmread('test2.layers');
-
-% Need at least one layer....
-layers = [layers(1,:) ; [0 0 0 0] ; layers(2,:)];
+layers = dlmread('test0.layers');
 
 % Change the units to Å
 layers(:,2) = layers(:,2) .* 1e-6;
 layers(:,3) = layers(:,3) .* 1e-6;
 
 % Read in the data.....
-data = dlmread('test2.dat');
+data = dlmread('test4.dat');
+datResol = data(:,4);
 
 % Group the Layers
 thick = layers(:,1);
 sld = complex(layers(:,2),layers(:,3));
 rough = layers(:,4);
 
-
-
-
-% Calculate reflectivity...
+% Calculate reflectivity....
 q = data(:,1);
 N = size(layers,1);
 ref = abelesSingle(q,N,thick,sld,rough);
+
+% Apply resolution....
+%resol = 0.035;
+%ref = resolutionPolly(q,ref,resol,length(q));
+ref = dataResolutionPolly(q,ref,datResol,length(q));
 
 % Plot the comparison....
 figure(1); clf
 semilogy(q,ref,'k-','LineWidth',2)
 hold on
-plot(data(:,1),data(:,2),'r.')
+plot(data(:,1),data(:,2),'ro')
 
 % Calculate the output....
 out = sum(sum((data(:,2) - ref).^2));

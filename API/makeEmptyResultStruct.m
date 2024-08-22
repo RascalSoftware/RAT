f75203e6aeedbb8c2ac33547b0d6a5bda2d7735b
Paul Sharp <44529197+DrPaulSharp@users.noreply.github.com>
2024-08-22 14:29:33 +0100

    Addresses review comment

--- API/makeEmptyResultStruct.m
+++ API/makeEmptyResultStruct.m
@@ -129,8 +129,8 @@ function result = makeEmptyResultStruct(nContrasts,nParams,domains)
         end
     end
     
-    fitParams = zeros(1,nParams);
-    coder.varsize('fitParams',[1 maxArraySize],[0 1]);
+    fitParams = zeros(nParams,1);
+    coder.varsize('fitParams',[maxArraySize 1],[1 0]);
     
     fitNames = cell(nParams,1);
     fitNamesChar = '';

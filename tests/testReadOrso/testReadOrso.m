7007b5c3c8de128bb6fc331479a0029b519e5446
alexhroom <alex.room@btinternet.com>
2025-02-12 14:55:54 +0000

    readOrso can now handle orso files with multiple datasets

--- tests/testReadOrso/testReadOrso.m
+++ tests/testReadOrso/testReadOrso.m
@@ -5,29 +5,29 @@ classdef testReadOrso < matlab.unittest.TestCase
         expected
     end
 
+    properties (TestParameter)
+        file = {'prist5_10K_m_025.Rqz', 'polref_data'}
+    end
+
     methods(TestClassSetup)
         function addDataPath(testCase)
             import matlab.unittest.fixtures.PathFixture
-            path = fullfile(getappdata(0, 'root'), 'tests', 'testProjectConversion');
+            path = fullfile(getappdata(0, 'root'), 'tests', 'testReadOrso');
             testCase.applyFixture(PathFixture(path))  
         end
-
-        function defineIO(testCase)
-            testCase.input = 'prist5_10K_m_025.Rqz.ort';
-            testCase.expected = 'expectedOrsoData.mat';
-        end
-
         function setWorkingFolder(testCase)
             import matlab.unittest.fixtures.WorkingFolderFixture;
             testCase.applyFixture(WorkingFolderFixture);
         end
     end
 
-    methods (Test)
-        function testOrsoReader(testCase)
-            expected_data = load('orsoData.mat').arr;
-            result = readOrso(testCase.input);
-            testCase.verifyEqual(expected_data, result)
+    methods (Test, ParameterCombination='exhaustive')
+        function testOrsoReader(testCase, file)
+            expected_data = load(append(file, ".mat")).arr;
+            result = readOrso(append(file, ".ort"));
+            for i=1:length(expected_data)
+                testCase.verifyEqual(expected_data{i}, result{i})
+            end
         end
     end
 

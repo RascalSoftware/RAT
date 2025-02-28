d9fa5e8b45b9a22cc175ceb7614129652266685e
Stephen Nneji <steve.nneji@gmail.com>
2025-02-28 10:42:56 +0000

    Adresses review comments

---
+++ utilities/validateParameter.m
@@ -0,0 +1,38 @@
+function param = validateParameter(param, paramList, paramDesc)
+    % Checks that the given parameter index or name is valid in the given list of parameters, then returns the
+    % parameter name from the list.
+    %
+    % Examples
+    % --------
+    % >> param = validateParameter('d2o', {'D2O', 'SMW'});
+    % This should return D2O as name check is case insensitive.
+    %
+    % Parameters
+    % ----------
+    % param
+    %     The parameter name or index to validate. 
+    % paramList
+    %     The list of parameter to check for param.
+    % paramDesc
+    %     The description or type of the parameter to find in the list.
+    arguments
+        param
+        paramList
+        paramDesc {mustBeTextScalar} = 'Parameter'
+    end
+
+    paramList = cellstr(paramList);
+    if isnumeric(param)
+        if (param < 1) || (param > length(paramList))
+            throw(exceptions.indexOutOfRange(sprintf('%s %d is out of range', paramDesc, param)));
+        else
+            param = paramList{param};
+        end
+    elseif isText(param)
+        found = strcmpi(param, paramList);
+        if ~any(found)
+            throw(exceptions.nameNotRecognised(sprintf('Unrecognised %s name %s', paramDesc, param)));
+        end
+        param = paramList{find(found, 1)};
+    end
+end

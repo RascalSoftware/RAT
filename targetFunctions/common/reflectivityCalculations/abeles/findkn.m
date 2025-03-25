ba040cd8d7ad0d9be689e59b97f0b9be90f53d4e
Paul Sharp <44529197+DrPaulSharp@users.noreply.github.com>
2025-03-25 10:58:39 +0000

    Addresses review comment

--- targetFunctions/common/reflectivityCalculations/abeles/findkn.m
+++ targetFunctions/common/reflectivityCalculations/abeles/findkn.m
@@ -1,6 +1,9 @@
 function kn = findkn(k0, sld)
 
 subtr = k0^2 - 4*pi*sld;
+
+% This is the sqrt function with branch cut in subtr from 0 to infinity
+% along a ray at angle theta = pi/2
 kn = exp(-0.25i*pi) * sqrt(subtr*exp(0.5i*pi));
 
 end

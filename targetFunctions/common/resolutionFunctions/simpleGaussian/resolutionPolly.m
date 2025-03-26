9777c5517ad00fcf0e5b3c59ef4d86ea6ab17ccb
Paul Sharp <44529197+DrPaulSharp@users.noreply.github.com>
2025-03-26 23:50:11 +0000

    Vectorises "resolutionPolly"

--- targetFunctions/common/resolutionFunctions/simpleGaussian/resolutionPolly.m
+++ targetFunctions/common/resolutionFunctions/simpleGaussian/resolutionPolly.m
@@ -5,19 +5,12 @@ simulation = zeros(points,1);
 
 for j = 1:points
 
-    sumg = 0;
     ilow = max(-10, -j + 1);
     ihi = min(10, points - j);
 
-    for i = ilow:ihi
-        g = exp(-1*((xdata(j+i)-xdata(j))/(resolutionValues(j)*xdata(j)))^2);
-        sumg = sumg + g;
-        simulation(j) = simulation(j) + rawSimulation(i+j) * g;
-    end
-    
-    if (sumg ~= 0)
-        simulation(j) = simulation(j) / sumg;
-    end
+    g = exp(-1*((xdata(j+ilow:j+ihi)-xdata(j))/(resolutionValues(j)*xdata(j))).^2);
+    sumg = sum(g);
+    simulation(j) = sum(rawSimulation(j+ilow:j+ihi) .* g) / sumg;
 
 end
 

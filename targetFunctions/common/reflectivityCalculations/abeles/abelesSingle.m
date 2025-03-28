6c7425fd1be1f03b6dc16394a05ff6083273bfc7
Paul Sharp <44529197+DrPaulSharp@users.noreply.github.com>
2025-03-28 15:44:25 +0000

    Reverses vectorisation and adds element-wise matrix multiplication

--- targetFunctions/common/reflectivityCalculations/abeles/abelesSingle.m
+++ targetFunctions/common/reflectivityCalculations/abeles/abelesSingle.m
@@ -13,36 +13,39 @@ nextSigmaSquared = layersSigma(2:end).^2;
 
 for points = 1:length(q)
 
-    M_n = eye(2, 'like', c1);
     M = eye(2, 'like', c1);
     k0 = 0.5 * q(points);
-
-    % Compute parameters for each layer
-    kn1 = findkn(k0, sld);
-    kn = [k0; kn1];
-       
-    % Find the Phase Factors = (k_n * d_n)
-    beta = kn .* layersThick;
-
-    % Construct an array of kn(1:end-1) and kn(2:end) for parameters based
-    % on combining consecutive values
-    kn_np1 = [kn(2:end) kn(1:end-1)];
-
-    numerator = diff(kn_np1, 1, 2);
-    denominator = sum(kn_np1, 2);
-    erf = exp(-2 * prod(kn_np1, 2) .* nextSigmaSquared);
-    r = (numerator ./ denominator) .* erf;
+    kn = complex(k0);
 
     for n = 1:N-1
 
-        % Create the M_n matrix
-        M_n(1,1) = exp(1i * beta(n));
-        M_n(1,2) = r(n) * exp(1i * beta(n));
-        M_n(2,1) = r(n) * exp(-1i * beta(n));
-        M_n(2,2) = exp(-1i * beta(n));
+        knp1 = findkn(k0, sld(n));
 
-        % Multiply the matrices
-        M = M * M_n;
+        % Find the Phase Factor = (k_n * d_n)
+        beta = kn * layersThick(n);
+
+        numerator = knp1 - kn;
+        denominator = knp1 + kn;
+        erf = exp(-2 * knp1 * kn * nextSigmaSquared(n));
+        r_n = (numerator / denominator) * erf;
+
+        % Multiply system transfer matrix by the layer transfer matrix
+        % Coder behaves better if you just do it manually
+        % rather than M = M * M_n;
+        % we'll denote the entries M = [a b; c d]
+        exp_beta = exp(1i*beta);
+        inv_exp_beta = exp(-1i*beta);
+
+        a_exp_beta = M(1,1) * exp_beta;
+        b_inv_exp_beta = M(1,2) * inv_exp_beta;
+        c_exp_beta = M(2,1) * exp_beta;
+        d_inv_exp_beta = M(2,2) * inv_exp_beta;
+ 
+        M = [a_exp_beta + r_n*b_inv_exp_beta r_n*a_exp_beta + b_inv_exp_beta;
+             c_exp_beta + r_n*d_inv_exp_beta r_n*c_exp_beta + d_inv_exp_beta];
+
+        % Set value of kn for the next layer
+        kn = knp1;
 
     end
 

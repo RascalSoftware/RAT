cc569a237c502c7706d283702b0c326f22140acf
arwel <arwel.hughes@stfc.ac.uk>
2025-06-24 10:12:46 +0100

    projectToJson matches the supplied json, but differs from an existing project - not sure why....

---
+++ jsonDevs/testMain.m
@@ -0,0 +1,26 @@
+
+
+% Main file for working on the converter....
+
+% 1. Make the project from a json file....
+problem = jsonToProject('DSPC_standard_layers.json');
+
+% Add the missing resolution (for this version...)
+for i = 1:problem.contrasts.numberOfContrasts
+    problem.setContrast(i,'Resolution','Resolution 1');
+end
+
+% % Also show the priors....
+% problem.showPriors = false;
+
+% Call the backwards conversion...
+projectToJson_new(problem,'TestFile');
+
+% 2. Converter from a normal project...
+standardLayersDSPCScript;
+
+% Call the conversion...
+projectToJson_new(problem,'TestFile');
+
+
+

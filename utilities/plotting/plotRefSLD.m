8491d67c590f8440cae14d7c2c2f301cd2063214
StephenNneji <34302892+StephenNneji@users.noreply.github.com>
2025-07-29 10:01:50 +0100

    Adds the shiftValue argument to plotRefSLD (#405)

--- utilities/plotting/plotRefSLD.m
+++ utilities/plotting/plotRefSLD.m
@@ -17,7 +17,8 @@ function plotRefSLD(project, result, options)
     %       * q4 (logical, default: false) indicates if the Y axis should plot Q^4.
     %       * showErrorBar (logical, default: true) indicates if the error bar should be shown.
     %       * showGrid (logical, default: false) indicates if the grid should be shown.
-    %       * showLegend (logical, default: true) indicates if the legend should be shown. 
+    %       * showLegend (logical, default: true) indicates if the legend should be shown.
+    %       * shiftValue (float, default: 100) A value between 1 and 100 that controls the spacing between the reflectivity plots for each of the contrasts.   
     
     arguments
         project
@@ -27,6 +28,7 @@ function plotRefSLD(project, result, options)
         options.showErrorBar {mustBeA(options.showErrorBar, 'logical')} = true
         options.showGrid {mustBeA(options.showGrid, 'logical')} = false
         options.showLegend {mustBeA(options.showLegend, 'logical')} = true
+        options.shiftValue {mustBeGreaterThanOrEqual(options.shiftValue, 1), mustBeLessThanOrEqual(options.shiftValue, 100)} = 100
     end
     % Convert the project class to a struct.
     controls = controlsClass();
@@ -43,5 +45,5 @@ function plotRefSLD(project, result, options)
     data.contrastNames = projectStruct.names.contrasts;
     
     plotRefSLDHelper(data, false, options.linearX, options.q4, options.showErrorBar, ...
-                     options.showGrid, options.showLegend);
+                     options.showGrid, options.showLegend, options.shiftValue);
 end

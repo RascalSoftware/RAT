604cad4408c9dcaea0a9e27afedbf02897ef46e4
arwel <arwel.hughes@stfc.ac.uk>
2026-02-03 13:56:24 +0000

    Added loadR2 utility

--- utilities/misc/saveR2.m
+++ utilities/misc/saveR2.m
@@ -14,22 +14,27 @@ end
 if exist(dirName,'dir') ~= 7
     mkdir(dirName);
 end
-currentDir = pwd;
-cd(dirName);
+currentDir = pwd; % Save the current directory name
 
-% Export the jsons...
-projectToJson(problem,'project.json');
-resultsToJson(results,'result.json');
-controlsToJson(controls,'controls.json');
-
-% Now do some custom file housekeeping. We need to copy these to our new
+% Do some custom file housekeeping. We need to copy these to our new
 % folder if there are any..
-cd(currentDir)
 filesTable = problem.customFile.varTable;
-
 for i = size(filesTable,1)
     thisFile = fullfile(filesTable{i,'Path'},filesTable{i,'Filename'});
     copyfile(thisFile,dirName)
+
+    % Change the paths of our custom files in projectClass to point to our
+    % new files..
+    problem.setCustomFile(i,'path',fullfile(currentDir,dirName));
 end
+
+% Go to our new directory and export the jsons...
+cd(dirName);
+projectToJson(problem,'project.json');
+resultsToJson(results,'results.json');
+controlsToJson(controls,'controls.json');
+
+% Go back to our original dir and we are done...
+cd(currentDir);
 
 end

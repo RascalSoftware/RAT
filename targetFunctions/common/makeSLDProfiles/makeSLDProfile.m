1b1d262b78cddfb82e101c3b3b37f24a9af3ff48
Paul Sharp <44529197+DrPaulSharp@users.noreply.github.com>
2026-06-19 16:54:36 +0100

    Replaces "normcdf" with inline function in SLD profile

--- targetFunctions/common/makeSLDProfiles/makeSLDProfile.m
+++ targetFunctions/common/makeSLDProfiles/makeSLDProfile.m
@@ -1,27 +1,26 @@
 function SLD = makeSLDProfile(bulkIn,bulkOut,layers,lastRough,nRepeats)
 
+% Scale the SLDs...
+layers(:,2) = layers(:,2) * 1e6;
 bulkIn = bulkIn * 1e6;
 bulkOut = bulkOut * 1e6;
+
+% Define cdf...
+cdf = @(x,mu,r) 0.5*(erf((x-mu)/(sqrt(2)*r)) + 1);
 
 if size(layers,1) > 0
     % Make a z range for the profile...
     % Find the maximum thickness, including any long roughness tail on final layer...
     totalThickness = sum(layers(:,1));
-    outerLayerCentre = totalThickness - (layers(end,1)/2);
 
     % Find the point which covers 99% of the outer error function..
-    outerLayerTailLim = (erfcinv(0.01) * sqrt(2) * layers(end,3)) + outerLayerCentre;
-
     % We need to make sure the total SLD range includes this...
-    outerLayerTailExtension = outerLayerTailLim - outerLayerCentre;
-    totalRange = ((totalThickness + outerLayerTailExtension)*nRepeats);
+    outerLayerTailExtension = (erfcinv(0.01) * sqrt(2) * layers(end,3));
+    totalRange = ((totalThickness + outerLayerTailExtension) * nRepeats);
 
     % Add some extra range at the end for bulk_out...
     totalRange = totalRange + 100;
     z = 0:totalRange;
-
-    % Scale the SLDs...
-    layers(:,2) = layers(:,2) * 1e6;
 
     % Repeat the stack according to 'nRepeats'...
     layers = repmat(layers,nRepeats,1);
@@ -34,18 +33,17 @@ if size(layers,1) > 0
     nLayers = size(layers,1);
     allFuncs = zeros(length(z),nLayers);
     alpha = zeros(1,nLayers);
-
     lastLayerSLD = bulkIn;
     thisPos = 50;
 
-    % Make the profile by adding an error function for each interface (we
-    % use 'normcdf' because it scales more easily than 'erf'...)
+    % Make the profile by adding an error function for each interface
+    % (we use 'cdf' because it scales more easily than 'erf'...)
     for i = 1:nLayers
         nextRough = layers(i,3);
         nextLayerSLD = layers(i,2);
         diff = nextLayerSLD - lastLayerSLD;
 
-        thisFun = normcdf(z,thisPos,nextRough);
+        thisFun = cdf(z,thisPos,nextRough);
         if diff < 0
             thisFun = -thisFun;
         end
@@ -63,13 +61,14 @@ else
     z = 0:100;
     pos = 50;
     diff = bulkOut - bulkIn;
-    thisFun = normcdf(z,pos,lastRough);
+    thisFun = cdf(z,pos,lastRough);
     if diff < 1
         thisFun = -thisFun;
     end
     total = thisFun * abs(diff);
 end
 
+% Scale the SLD's back to Angstroms...
 total = (total + bulkIn) * 1e-6;
 SLD = [z(:) total(:)];
 
